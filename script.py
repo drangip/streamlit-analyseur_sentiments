4e383899194d52a440e411638a0451c6a704814d
Florent Pignard <pignardf@gmail.com>
2025-11-23 21:19:06 +0100

    modifiction du script

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import streamlit
+import streamlit as st
 from transformers import pipeline
 
 st.title("Analyseur de Sentiments")
@@ -8,5 +8,5 @@ text = st.text_input("Entrez le texte à analyser :")
 sentiment_analyzer = pipeline("sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
 
 if text:
-    result = sentiment_analyzer(texte)[0]
+    result = sentiment_analyzer(text)[0]
     st.write(f"Label: {resultat['label']}, Score: {resultat['score']:.4f}")
